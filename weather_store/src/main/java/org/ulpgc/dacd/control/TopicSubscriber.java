1b3cc23d33c840e4b3481f5bc02ef7fefb7c681d
Davidgarve <davidgaver2004@gmail.com>
2023-12-09 13:36:31 +0000

    TopicSubscriber Class simplified

--- weather_store/src/main/java/org/ulpgc/dacd/control/TopicSubscriber.java
+++ weather_store/src/main/java/org/ulpgc/dacd/control/TopicSubscriber.java
@@ -4,40 +4,34 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
-    public class TopicSubscriber implements Subscriber{
+public class TopicSubscriber implements Subscriber {
 
-        public void start() {
-            String brokerURL = "tcp://localhost:61616";
-            String topicName = "prediction.weather";
-            String subscriberId = "weather_store";
+    public void start() {
+        String brokerURL = "tcp://localhost:61616";
+        String topicName = "prediction.weather";
+        String subscriberId = "weather_store";
 
-            try {
-                ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
-                Connection connection = connectionFactory.createConnection();
-                connection.setClientID(subscriberId);
+        try {
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
+            Connection connection = connectionFactory.createConnection();
+            connection.setClientID(subscriberId);
 
-                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                Topic topic = session.createTopic(topicName);
-                javax.jms.TopicSubscriber subscriber = session.createDurableSubscriber(topic, subscriberId);
-                connection.start();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic = session.createTopic(topicName);
+            javax.jms.TopicSubscriber subscriber = session.createDurableSubscriber(topic, subscriberId);
+            connection.start();
 
-                while (true) {
-                    Message message = subscriber.receive();
-                    if (message instanceof TextMessage) {
-                        TextMessage textMessage = (TextMessage) message;
-                        System.out.println("Received message: " + textMessage.getText());
-                        storeEvent(textMessage.getText());
-
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        private static void storeEvent(String jsonWeather) {
             EventStore eventStore = new EventStore();
-            eventStore.storeEvent(jsonWeather);
+            while (true) {
+                Message message = subscriber.receive();
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    System.out.println("Received message: " + textMessage.getText());
+                    eventStore.storeEvent(textMessage.getText());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
+}
