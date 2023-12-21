86c45c5131a7f746a825fdee9e0278b0b0f729c8
Davidgarve <davidgaver2004@gmail.com>
2023-12-21 16:50:38 +0000

    HotelEventPublisher and HotelSupplier interfaces implemented

--- hotel_provider/src/main/java/org/ulpgc/dacd/control/HotelEventPublisher.java
+++ hotel_provider/src/main/java/org/ulpgc/dacd/control/HotelEventPublisher.java
@@ -2,6 +2,7 @@ package org.ulpgc.dacd.control;
 
 import org.ulpgc.dacd.model.Hotel;
 
+
 public interface HotelEventPublisher {
     void sendHotelToBroker(Hotel hotel, String brokerURL, String topicName);
 }
--- hotel_provider/src/main/java/org/ulpgc/dacd/control/HotelSupplier.java
+++ hotel_provider/src/main/java/org/ulpgc/dacd/control/HotelSupplier.java
@@ -5,6 +5,7 @@ import org.ulpgc.dacd.model.Hotel;
 import java.time.Instant;
 import java.util.List;
 
+
 public interface HotelSupplier {
     List<Hotel> getHotel(String hotelName, String checkInDate, String checkOutDate, String location, Instant ts);
 }
