c1f94b9886c4f6f5add3503496203db964bd582c
Davidgarve <davidgaver2004@gmail.com>
2023-12-27 17:35:06 +0000

    Locations changed and toString method added

--- weather_provider/src/main/java/org/ulpc/dacd/control/Main.java
+++ weather_provider/src/main/java/org/ulpc/dacd/control/Main.java
@@ -7,21 +7,21 @@ public class Main {
         String apiKey = args[0];
         OpenWeatherMapSupplier supplier = new OpenWeatherMapSupplier(apiKey);
 
-        Location caletaDeSebo = new Location(29.23036, -13.50536, "La Graciosa");
-        Location arrecife = new Location(28.966271, -13.545968, "Lanzarote");
-        Location puertoDelRosario = new Location(28.49998, -13.86823, "Fuerteventura");
-        Location lasPalmas = new Location(28.12144, -15.43979, "Gran Canaria");
-        Location santaCruzTenerife = new Location(28.46140, -16.25852, "Tenerife");
-        Location sanSebastian = new Location(28.09274, -17.11118, "La Gomera");
-        Location santaCruzLaPalma = new Location(28.68369, -17.76615, "La Palma");
-        Location valverde = new Location(27.808402, -17.915343, "El Hierro");
+        Location madrid = new Location(40.41581172250397, -3.6927121960696088,  "Madrid");
+        Location barcelona = new Location(41.387120696877425, 2.1960018069652363, "Barcelona");
+        Location valencia = new Location(39.47266732471288, -0.36124752700365187, "Valencia");
+        Location oviedo = new Location(43.36293697609066, -5.8563961741044075, "Oviedo");
+        Location marbella = new Location(36.50799477649729, -4.905502059451248, "Marbella");
+        Location mallorca = new Location(39.565888338910476, 2.652656263464681, "Mallorca");
+        Location bilbao = new Location(43.26725507112307, -2.9341964108977976, "Bilbao");
+        Location tenerife = new Location(28.12094382231223, -16.774909594488363, "Tenerife");
 
         String brokerURL = "tcp://localhost:61616";
         String topicName = "prediction.weather";
 
         JmsWeatherStore jmsWeatherStore = new JmsWeatherStore();
         WeatherController weatherController = new WeatherController(supplier, jmsWeatherStore, brokerURL, topicName);
-        weatherController.execute(caletaDeSebo, arrecife, puertoDelRosario, lasPalmas, santaCruzTenerife, sanSebastian, santaCruzLaPalma, valverde);
+        weatherController.execute(madrid, barcelona, valencia, oviedo, marbella, mallorca, bilbao, tenerife);
     }
 
 
--- weather_provider/src/main/java/org/ulpc/dacd/model/Weather.java
+++ weather_provider/src/main/java/org/ulpc/dacd/model/Weather.java
@@ -7,15 +7,15 @@ public record Weather(double pop, double windSpeed, double temp, int humidity, S
     @Override
     public String toString() {
         return "Weather{" +
-                "pop=" + pop() +
-                ", windSpeed=" + windSpeed() +
-                ", temperature=" + temp() +
-                ", humidity=" + humidity() +
-                ", predictionDate='" + predictionDate() + '\'' +
-                ", clouds=" + clouds() +
-                ", ts=" + ts() +
-                ", ss=" + ss() +
-                ", location=" + location().toString() +
+                "pop=" + pop +
+                ", windSpeed=" + windSpeed +
+                ", temp=" + temp +
+                ", humidity=" + humidity +
+                ", predictionDate='" + predictionDate + '\'' +
+                ", ss='" + ss + '\'' +
+                ", clouds=" + clouds +
+                ", ts=" + ts +
+                ", location=" + location +
                 '}';
     }
 }
