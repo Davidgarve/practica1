81bae236a780e1cd55c68c469d8972bf4bf40494
Davidgarve <davidgaver2004@gmail.com>
2024-01-09 10:21:46 +0000

    code cleaned

--- vacation_visionary/src/main/java/org/ulpgc/dacd/control/EventStore.java
+++ vacation_visionary/src/main/java/org/ulpgc/dacd/control/EventStore.java
@@ -1,9 +1,5 @@
 package org.ulpgc.dacd.control;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
-
 public interface EventStore {
     void createTables();
     void saveHotel(String json);
